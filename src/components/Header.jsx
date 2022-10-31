77ab9e7d45dddf15adaed527cb0c06066113eb30
Kristian Hartmann <hartkrist@gmail.comm>
2022-10-31 16:01:25 +0100

    fix

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "/src/assets/schoolhacks-logo.png";
+import "./styles/main.css";
+
+<header className="container">
+  <Link to={`/`}>
+    <img
+      className="logo"
+      src={logo}
+      Link
+      alt="SchoolHacks logo"
+      title="SchoolHacks | Home"
+    />
+  </Link>
+  <nav>{<Link to={`/workshops`}>Workshops</Link>}</nav>
+  <nav>{<Link to={`/`}>Home</Link>}</nav>
+  <nav>{<Link to={`/about`}>About</Link>}</nav>
+</header>;
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import logo from "../src/assets/schoolhacks-logo.png";
-import './styles/main.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./styles/main.css";
 import Home from "./components/Home.jsx";
+import Header from "./components/Header.jsx";
 import Workshops from "./components/Workshops";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-        <div className="main">
-            <header className="container">
-                <img
-                    className="logo"
-                    src={logo}
-                    alt="SchoolHacks logo"
-                    title="SchoolHacks | Home"
-                />
-                <nav>
-                    {/*Link*/}
-                </nav>
-            </header>
-
-            <Routes>
-                <Route path="/" element={<Home titel="Welcome to SchoolHacks"/>}/>
-                <Route path="/workshops" element={<Workshops/>}/>
-            </Routes>
-
-            <footer className="container">
-                &copy;2022 | <a
-                href="https://www.schoolhacks.eu"
-                target="_blank"
-                rel="noreferrer noopener"
-            >SchoolHacks</a>
-            </footer>
-        </div>
-        </BrowserRouter>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home titel="Welcome to SchoolHacks" />} />
+          <Route path="/workshops" element={<Workshops />} />
+        </Routes>
+        <footer className="container">
+          &copy;2022 |{" "}
+          <a
+            href="https://www.schoolhacks.eu"
+            target="_blank"
+            rel="noreferrer noopener">
+            SchoolHacks
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>
+);
